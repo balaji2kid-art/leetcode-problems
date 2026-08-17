5eb33f623336a28eb6c8cd85677ba4213a73dd6f
BALAJI A <balaji2kid@gmail.com>
2026-08-17 11:50:36 +0530

    Time: 0 ms (100%), Space: 42 MB (66.26%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+
+        int total = m + n - 2;
+
+        // choose smaller value for efficient calculation
+        int r = Math.min(m - 1, n - 1);
+
+        long ans = 1;
+
+        for(int i = 1; i <= r; i++) {
+            ans = ans * (total - r + i) / i;
+        }
+
+        return (int) ans;
+    }
+}
